0dfaec5f5e6b9d440430bc7f1b3d2d1b68f9329d
Christopher Young <chrisyoungplus@outlook.com>
2020-07-18 02:52:12 -0500

    Imported to maven and updated to PaperMC 1.16.1

---
+++ src/ws/factions/wilderness/ProMaintenanceMode/Paper/Main/MMArgument.java
@@ -0,0 +1,13 @@
+package ws.factions.wilderness.ProMaintenanceMode.Paper.Main;
+
+import org.bukkit.command.CommandSender;
+
+public interface MMArgument 
+{
+	String getHelp();
+	boolean useByPlayerOnly();
+	String getArgumentName();
+	String getDescription();
+	void executeCommand(CommandSender sender, String label, String[] args);
+	String getPermission();
+}
